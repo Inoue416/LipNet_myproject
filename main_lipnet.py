85d103760f1a681d0fb7786c67f357d40fc8a7bf
Yuya Inoue <yuyatuta@icloud.com>
2023-04-08 12:23:39 +0900

    dataset_lipnetやmodel_lipnetの変更に対応 動作確認済み

--- main_lipnet.py
+++ main_lipnet.py
@@ -15,7 +15,11 @@ import options_lipnet as opt
 if(__name__ == '__main__'):
     opt = __import__('options_lipnet')
     os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu
-    writer = SummaryWriter(log_dir=opt.log_dir) # ダッシュボード作成
+    logdir = os.path.join(opt.save_folder_root, 
+                        '_'.join([str(s) for s in [opt.year, opt.month, opt.day]]), opt.log_dir)
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+    writer = SummaryWriter(log_dir=logdir) # ダッシュボード作成
 
 # データセットをDataLoaderへ入れてDataLoaderの設定をして返す
 def dataset2dataloader(dataset, num_workers=opt.num_workers, shuffle=True):
@@ -48,7 +52,9 @@ def test(model, net):
             opt.val_list,
             opt.vid_padding,
             opt.txt_padding,
-            'test')
+            'test', 
+            opt.anno_kind,
+            opt.color_mode)
 
         #print('num_test_data:{}'.format(len(dataset.data)))
         model.eval() # テストモードへ
@@ -80,7 +86,7 @@ def test(model, net):
             cer.extend(MyDataset.cer(pred_txt, truth_txt))
 
             # 条件の回数の時だけエラー率などを表示
-            if(i_iter % 100 == 0):
+            if(i_iter % opt.test_display == 0):
                 v = 1.0*(time.time()-tic)/(i_iter+1)
                 eta = v * (len(loader)-i_iter) / 3600.0
 
@@ -92,7 +98,8 @@ def test(model, net):
                 print(''.join(101 *'-'))
                 print('test_iter={},loss={},eta={},wer={},cer={}'.format(i_iter,loss,eta,np.array(wer).mean(),np.array(cer).mean()))
                 print(''.join(101 *'-'))
-
+            break
+        print('\n\ntest finish time: {:.2f} s\n\n'.format(time.time() - tic))
         return (np.array(loss_list).mean(), np.array(wer).mean(), np.array(cer).mean())
 
 # 訓練
@@ -104,14 +111,16 @@ def train(model, net):
         opt.train_list,
         opt.vid_padding,
         opt.txt_padding,
-        'train')
+        'train',
+        opt.anno_kind,
+        opt.color_mode)
 
     # DataLoaderの作成
     loader = dataset2dataloader(dataset)
     # optimizerの初期化(Adam使用)
     optimizer = optim.Adam(model.parameters(),
                 lr = opt.base_lr,
-                weight_decay = 0,#.001,#0.01,#0.1, # パラメータのL2ノルムを正規化としてどれくらい用いるから指定
+                weight_decay = opt.decay,   # パラメータのL2ノルムを正規化としてどれくらい用いるから指定
                 eps=1e-8,
                 amsgrad = True)# AMSgradを使用する"""
     """optimizer = optim.SGD(model.parameters(),
@@ -126,13 +135,13 @@ def train(model, net):
     train_cer = []
     loss_list = []
     for epoch in range(opt.max_epoch): # epoch分学習する
+        epoch_tic = time.time()
         for (i_iter, input) in enumerate(loader):
             model.train() # 訓練モードへ
             vid = input.get('vid').cuda()
             txt = input.get('txt').cuda()
             vid_len = input.get('vid_len').cuda()
             txt_len = input.get('txt_len').cuda()
-
             #if vid_len.view(-1) < txt_len.view(-1):
             #print('vl : {}'.format(vid_len.view(-1)))
             #print('tl : {}'.format(txt_len.view(-1)))
@@ -146,7 +155,6 @@ def train(model, net):
             #print(loss)
             # 損出をもとにバックワードで学習
             loss.backward()
-
             if(opt.is_optimize):
                 optimizer.step() # gradプロパティに学習率をかけてパラメータを更新
 
@@ -160,12 +168,10 @@ def train(model, net):
             train_cer.extend(MyDataset.cer(pred_txt, truth_txt))
 
             # 条件の回数の時、それぞれの経過を表示
-            if(tot_iter % opt.display == 0):
-                v = 1.0*(time.time()-tic)/(tot_iter+1)
-                eta = (len(loader)-i_iter)*v/3600.0
-
+            if(tot_iter % opt.train_display == 0):
                 writer.add_scalar('train loss', loss, tot_iter)
                 writer.add_scalar('train wer', np.array(train_wer).mean(), tot_iter)
+                writer.add_scalar('train cer', np.array(train_cer).mean(), tot_iter)
                 print(''.join(101*'-'))
                 print('{:<50}|{:>50}'.format('predict', 'truth'))
                 print(''.join(101*'-'))
@@ -173,15 +179,8 @@ def train(model, net):
                 for (predict, truth) in list(zip(pred_txt, truth_txt))[:3]:
                     print('{:<50}|{:>50}'.format(predict, truth))
                 print(''.join(101*'-'))
-                print('epoch={},tot_iter={},base_lr={},eta={},loss_mean={},loss={},train_wer={},train_cer={}'.format(epoch, tot_iter, opt.base_lr, eta, torch.mean(torch.stack(loss_list)), loss, np.array(train_wer).mean(), np.array(train_cer).mean()))
+                print('epoch={},tot_iter={},base_lr={},loss_mean={}loss={},train_wer={},train_cer={}'.format(epoch, tot_iter, opt.base_lr, torch.mean(torch.stack(loss_list)), loss, np.array(train_wer).mean(), np.array(train_cer).mean()))
                 print(''.join(101*'-'))
-                if (tot_iter % 3000 == 0):
-                    savename = 'base_lr{}_{}_loss_{}_wer_{}_cer_{}.pt'.format(opt.base_lr, opt.save_prefix, torch.mean(torch.stack(loss_list)),  np.array(train_wer).mean(), np.array(train_cer).mean()) # 学習した重みを保存するための名前を作成
-                    (path, name) = os.path.split(savename)
-                    if(not os.path.exists(path)): os.makedirs(path) # 重みを保存するフォルダを作成する
-                    torch.save(model.state_dict(), savename) # 学習した重みを保存
-                if(not opt.is_optimize):
-                    exit()
 
             if(tot_iter % opt.test_step == 0):
                 (loss, wer, cer) = test(model, net)
@@ -190,14 +189,33 @@ def train(model, net):
                 writer.add_scalar('val loss', loss, tot_iter)
                 writer.add_scalar('wer', wer, tot_iter)
                 writer.add_scalar('cer', cer, tot_iter)
-                #savename = 'base_lr{}_{}_loss_{}_wer_{}_cer_{}.pt'.format(opt.base_lr, opt.save_prefix, loss,  wer, cer) # 学習した重みを保存するための名前を作成
-                #(path, name) = os.path.split(savename)
-                #if(not opt.is_optimize):
-                    #exit()
+
+            if (tot_iter % opt.save_step == 0):
+                if not os.path.exists(os.path.join(opt.save_folder_root, '_'.join([str(s) for s in [opt.year, opt.month, opt.day]]))):
+                    os.makedirs(os.path.join(opt.save_folder_root, '_'.join([str(s) for s in [opt.year, opt.month, opt.day]])))
+                savename = 'base_lr{}{}_loss_{}_wer_{}_cer_{}.pt'.format(opt.base_lr, opt.save_prefix, torch.mean(torch.stack(loss_list)),  np.array(train_wer).mean(), np.array(train_cer).mean()) # 学習した重みを保存するための名前を作成
+                # print(savename)
+                (path, name) = os.path.split(savename)
+                # print(path)
+                # print(name)
+                
+                if(not os.path.exists(os.path.join(opt.save_folder_root, '_'.join([str(s) for s in [opt.year, opt.month, opt.day]]), path))):
+                    os.makedirs(os.path.join(opt.save_folder_root, '_'.join([str(s) for s in [opt.year, opt.month, opt.day]]), path)) # 重みを保存するフォルダを作成する
+                torch.save(model.state_dict(), 
+                            os.path.join(opt.save_folder_root, '_'.join([str(s) for s in [opt.year, opt.month, opt.day]]), path, name+'.pt')) # 学習した重みを保存
+                
+            if(not opt.is_optimize):
+                exit()
+
+        epoch_end_tic = time.time() - epoch_tic
+        print('\n\nepoch{} finish time: {:.2f} s\n\n'.format(epoch, epoch_end_tic))
+    
+    print('\n\nAll process finish.')
+    print('time: {:.2f} s'.format(time.time() - tic))
 
 if(__name__ == '__main__'):
     print("Loading options...")
-    model = LipNet() # モデルの定義
+    model = LipNet(color_mode=0, dropout_p=opt.dropout_p) # モデルの定義
     model = model.cuda() # gpu使用
     net = nn.DataParallel(model).cuda() # データの並列処理化
 
